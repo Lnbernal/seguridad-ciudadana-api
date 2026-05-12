cb874bd1056ca6cd4b5a3820ec49de1106d744dc
lnbernal <luis.nicolas.bernal@gmail.com>
2026-05-12 00:03:14 -0500

    se sube la base para comprobar en web

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 //funcionamiento local
-const { Sequelize } = require('sequelize');
+/*const { Sequelize } = require('sequelize');
 
 require('dotenv').config();
 
@@ -24,8 +24,8 @@ const sequelize = new Sequelize(
 );
 
 module.exports = sequelize;
-
-/*funcionamiento en web
+*/
+//funcionamiento en web
 
 const { Sequelize } = require('sequelize');
 
@@ -59,4 +59,4 @@ const sequelize = new Sequelize(
 
 );
 
-module.exports = sequelize; */
+module.exports = sequelize; 
